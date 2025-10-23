eb784cb72113243ad65283351f1ad07f3d736ef7
ZovaTech4 <zovatech.id@gmail.com>
2025-10-23 19:05:41 +0700

    kamis

--- Mysql/open-api.sql
+++ Mysql/open-api.sql
@@ -66,19 +66,19 @@ from webhook_endpoints;
 
 select active, disabled, active_user, lu.*
 from leadspeek_users as lu
-where leadspeek_api_id = 26778152;
+where leadspeek_api_id = 37362347;
 
 select *
 from topup_campaigns
-where leadspeek_api_id = 29484235;
+where leadspeek_api_id = 37362347;
 
 SELECT * 
 FROM `leadspeek_invoices` 
-where leadspeek_api_id = 26778152;
+where leadspeek_api_id = 37362347;
 
 SELECT * 
 FROM `leadspeek_reports` 
-where leadspeek_api_id = 26778152;
+where leadspeek_api_id = 37362347;
 
 select * 
 from jobs;
@@ -86,6 +86,91 @@ from jobs;
 select *
 from users
 where company_id = 165;
+
+SELECT 
+	id,person_id,lp_user_id,company_id,user_id,topup_id,leadspeek_api_id,invoice_id,
+    CONVERT(AES_DECRYPT(FROM_bASE64(email), '8e651522e38256f2') USING utf8mb4) as email,
+    CONVERT(AES_DECRYPT(FROM_bASE64(email2), '8e651522e38256f2') USING utf8mb4) as email2,
+    original_md5,ipaddress,
+    CONVERT(AES_DECRYPT(FROM_bASE64(source), '8e651522e38256f2') USING utf8mb4) as source,
+    optindate,clickdate,
+	CONVERT(AES_DECRYPT(FROM_bASE64(referer), '8e651522e38256f2') USING utf8mb4) as referer,
+	CONVERT(AES_DECRYPT(FROM_bASE64(phone), '8e651522e38256f2') USING utf8mb4) as phone,
+	CONVERT(AES_DECRYPT(FROM_bASE64(phone2), '8e651522e38256f2') USING utf8mb4) as phone2,
+	CONVERT(AES_DECRYPT(FROM_bASE64(firstname), '8e651522e38256f2') USING utf8mb4) as firstname,
+	CONVERT(AES_DECRYPT(FROM_bASE64(lastname), '8e651522e38256f2') USING utf8mb4) as lastname,
+	CONVERT(AES_DECRYPT(FROM_bASE64(address1), '8e651522e38256f2') USING utf8mb4) as address1,
+	CONVERT(AES_DECRYPT(FROM_bASE64(address2), '8e651522e38256f2') USING utf8mb4) as address2,
+	CONVERT(AES_DECRYPT(FROM_bASE64(city), '8e651522e38256f2') USING utf8mb4) as city,
+	CONVERT(AES_DECRYPT(FROM_bASE64(state), '8e651522e38256f2') USING utf8mb4) as state,
+	CONVERT(AES_DECRYPT(FROM_bASE64(zipcode), '8e651522e38256f2') USING utf8mb4) as zipcode,
+	price_lead,platform_price_lead,root_price_lead,keyword,description,active,lead_from,updated_at,created_at,encrypted
+FROM `leadspeek_reports` where leadspeek_api_id = 29484235;
+
+
+SELECT 
+	id,person_id,lp_user_id,company_id,user_id,topup_id,leadspeek_api_id,invoice_id,
+    CONVERT(AES_DECRYPT(FROM_bASE64(email), '8e651522e38256f2') USING utf8mb4) as email,
+    CONVERT(AES_DECRYPT(FROM_bASE64(email2), '8e651522e38256f2') USING utf8mb4) as email2,
+    original_md5,ipaddress,
+    CONVERT(AES_DECRYPT(FROM_bASE64(source), '8e651522e38256f2') USING utf8mb4) as source,
+    optindate,clickdate,
+	CONVERT(AES_DECRYPT(FROM_bASE64(referer), '8e651522e38256f2') USING utf8mb4) as referer,
+	CONVERT(AES_DECRYPT(FROM_bASE64(phone), '8e651522e38256f2') USING utf8mb4) as phone,
+	CONVERT(AES_DECRYPT(FROM_bASE64(phone2), '8e651522e38256f2') USING utf8mb4) as phone2,
+	CONVERT(AES_DECRYPT(FROM_bASE64(firstname), '8e651522e38256f2') USING utf8mb4) as firstname,
+	CONVERT(AES_DECRYPT(FROM_bASE64(lastname), '8e651522e38256f2') USING utf8mb4) as lastname,
+	CONVERT(AES_DECRYPT(FROM_bASE64(address1), '8e651522e38256f2') USING utf8mb4) as address1,
+	CONVERT(AES_DECRYPT(FROM_bASE64(address2), '8e651522e38256f2') USING utf8mb4) as address2,
+	CONVERT(AES_DECRYPT(FROM_bASE64(city), '8e651522e38256f2') USING utf8mb4) as city,
+	CONVERT(AES_DECRYPT(FROM_bASE64(state), '8e651522e38256f2') USING utf8mb4) as state,
+	CONVERT(AES_DECRYPT(FROM_bASE64(zipcode), '8e651522e38256f2') USING utf8mb4) as zipcode,
+	price_lead,platform_price_lead,root_price_lead,keyword,description,active,lead_from,updated_at,created_at,encrypted
+FROM `leadspeek_reports` where leadspeek_api_id = 29484235;
+
+
+select * from leadspeek_users where leadspeek_api_id = 75935502;
+
+select * from person_advance_1;
+
+
+select clickdate, lu.* 
+from leadspeek_reports as lu
+where 
+	leadspeek_api_id = 43919974 and 
+	DATE_FORMAT(clickdate, "%Y-%m-%d") <= '2023-05-09';
+
+select * from person_b2b;
+select * from persons where id = 7807;
+
+SELECT DATE_FORMAT('', '%Y-%m-%d') AS formatted;
+
+SELECT 
+    `lr`.`id`, 
+    `lu`.`leadspeek_type`
+FROM 
+    `leadspeek_reports` AS `lr`
+INNER JOIN 
+    `leadspeek_users` AS `lu` 
+    ON `lu`.`leadspeek_api_id` = `lr`.`leadspeek_api_id`
+WHERE 
+    `lr`.`id` = '1404951756889595'
+    AND `lr`.`active` = 'T'
+    AND `lu`.`user_id` IN (282)
+    AND `lu`.`archived` = 'F'
+LIMIT 1;
+
+select * from users where company_parent = 464 and user_type = 'client' and active = 'T';
+select * from leadspeek_users where company_id = 464 and archived = 'F';
+select * from users where company_id = 164 and user_type = 'userdownline';
+
+select * 
+from leadspeek_reports 
+join leadspeek_users on leadspeek_users.leadspeek_api_id = leadspeek_reports.leadspeek_api_id
+where leadspeek_reports.user_id in (
+	select id from users where company_parent = 464 and user_type = 'client' and active = 'T'
+) and leadspeek_reports.active = 'T' and leadspeek_users.archived = 'F';
+
 
 
 
