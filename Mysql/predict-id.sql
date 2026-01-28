1f028d9c7f41512c7c3b3bce5622615032cad7a3
Feyfa <fisikamodern00@gmail.com>
2026-01-28 21:23:22 +0700

    s

--- Mysql/predict-id.sql
+++ Mysql/predict-id.sql
@@ -92,7 +92,7 @@ where
 	feature_id in (4,5,6) and 
 	user_id in (select id from users where company_id in (164) and user_type in ('user','userdownline'));
 
-select * from user_logs where created_at > '2026-01-26 23:33:33' order by id desc;
+select * from user_logs order by id desc;
 
 select * from user_logs where action like '%marketing service%' order by id desc;
 
