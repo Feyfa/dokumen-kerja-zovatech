e374669f3595890d909bfc3919ea79e7eb7276c2
ZovaTech4 <zovatech.id@gmail.com>
2026-02-24 12:01:24 +0700

    asdasd

--- Mysql/3d-secure-credit-card.sql
+++ Mysql/3d-secure-credit-card.sql
@@ -4,3 +4,5 @@ select * from leadspeek_users where leadspeek_api_id = @leadspeek_api_id;
 select * from topup_campaigns where leadspeek_api_id = @leadspeek_api_id;
 select * from leadspeek_invoices where leadspeek_api_id = @leadspeek_api_id;
 select * from leadspeek_reports where leadspeek_api_id = @leadspeek_api_id;
+
+select user_type, payment_status, u.* from users as u where id in (279,282);
