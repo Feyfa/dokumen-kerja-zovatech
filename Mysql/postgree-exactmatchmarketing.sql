116582576129bc17b7f16742f1b441086a25dd2b
ZovaTech4 <zovatech.id@gmail.com>
2026-04-01 14:56:50 +0700

    asd

--- Mysql/postgree-exactmatchmarketing.sql
+++ Mysql/postgree-exactmatchmarketing.sql
@@ -4,3 +4,12 @@ select * from migrations;
 
 select  * from contacts;
 select  * from contact_lists;
+
+select * from module_jobs;
+select * from jobs;
+
+select * from campaigns;
+
+select * from audit_logs order by id desc;
+
+select * from campaigns;
