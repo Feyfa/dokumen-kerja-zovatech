dc2827721e9053a23863367f46f12f5a83dd7cb1
ZovaTech4 <zovatech.id@gmail.com>
2026-04-02 17:37:31 +0700

    asd

--- temp/index.php
+++ temp/index.php
@@ -1,24 +1,5 @@
 <?php
 
-echo md5("jidan@gmail.com") . PHP_EOL;
-echo md5("JIDAN@GMAIL.COM") . PHP_EOL;
+$value = base64_decode("eyJjYW1wYWlnbkNvZGUiOiI2MzYyMTE3MCIsImN1c3RvbVBhcmFtcyI6ImFnaWVzX2JhaWsiLCJwYWdlVXJsIjoiaHR0cDovL2xvY2FsaG9zdC90ZXN0LXBpeGVsLyJ9");
 
-foreach ($otherEmails as $index => $be) {
-    if (trim($be) != "") {
-        // ========== check ke supression list ==========
-        // code ...
-        // code ...
-        // ========== check ke supression list ==========
-        
-        // ========== check ke opt out list ==========
-        // code ...
-        // ========== check ke opt out list ==========
-        
-        // ========== check ke truelist ==========
-        // code ...
-        // ========== check ke truelist ==========
-    }
-}
-
-
-
+var_dump($value);
