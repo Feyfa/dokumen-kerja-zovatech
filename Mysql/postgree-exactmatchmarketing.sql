09581cdd269886f56f87ab58d556e9ffc8ee6146
ZovaTech4 <zovatech.id@gmail.com>
2026-04-24 18:39:48 +0700

    tau ah

--- Mysql/postgree-exactmatchmarketing.sql
+++ Mysql/postgree-exactmatchmarketing.sql
@@ -35,7 +35,21 @@ select * from campaign_integrations;
 select * from integration_field_mappings;
 
 select * from export_templates;
-select * from module_jobs;
 select * from export_templates where id = '019db443-6d71-718c-b785-1c7e07c6b2f5'
 
 -- user_3CJ3sadWfYOORa4sQc3XjdBILit
+
+
+
+
+
+
+
+
+
+select * from module_jobs;
+
+
+select id, metadata->>'campaign_name' as campaign_name 
+from module_jobs 
+where module = 'site_id';
