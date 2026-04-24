2bedecd2b116de0468b1ab5c3c676b2b7381095c
ZovaTech4 <zovatech.id@gmail.com>
2026-04-24 18:39:25 +0700

    asdasd

--- Mysql/postgree-exactmatchmarketing.sql
+++ Mysql/postgree-exactmatchmarketing.sql
@@ -15,7 +15,7 @@ select * from users;
 select * from organizations;
 select * from organization_credit_balances;
 select * from campaigns;
-select * from contacts;
+select * from contacts order by id asc;
 select * from credit_transactions;
 select * from webhook_events we ;
 
@@ -33,5 +33,9 @@ select * from integration_push_logs;
 select * from campaign_integrations;
 
 select * from integration_field_mappings;
+
+select * from export_templates;
+select * from module_jobs;
+select * from export_templates where id = '019db443-6d71-718c-b785-1c7e07c6b2f5'
 
 -- user_3CJ3sadWfYOORa4sQc3XjdBILit
--- temp/index.php
+++ temp/index.php
@@ -1,8 +1,8 @@
 <?php
 
 $payload = [
-    'campaignCode' => '84632251',  // ganti dengan campaign_code yang ada di DB
-    'pageUrl'=> 'https://chatgpt.com',
+    'campaignCode' => '84253512',  // ganti dengan campaign_code yang ada di DB
+    'pageUrl'=> 'https://jidan.com',
 ];
 $label = base64_encode(json_encode($payload));
 echo "label: " . $label . PHP_EOL;
